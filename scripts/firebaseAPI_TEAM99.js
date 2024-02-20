3dc37a4fa91134cc7f83f4f07cbd0827f34315bd
Vincent Fung <vfung26@my.bcit.ca>
2024-02-20 13:23:08 -0800

    Added some stuff

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDaYpuQD4bO7ZCea-51jZKipREpT5AIKNw",
+    authDomain: "comp1800-202410-4bff5.firebaseapp.com",
+    projectId: "comp1800-202410-4bff5",
+    storageBucket: "comp1800-202410-4bff5.appspot.com",
+    messagingSenderId: "1015179406661",
+    appId: "1:1015179406661:web:2d102e816572ee22bae22c",
+    measurementId: "G-ELCN9FHPSR"
 };
 
 //--------------------------------------------
